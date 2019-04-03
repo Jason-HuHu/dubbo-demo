0178252d1ae5c81e91635ba9c6595f26a60953ab
huxl <1259450641@qq.com>
2019-04-03 09:28:43 +0800

    fix 完善代码格式

--- dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/impl/DemoServerImpl.java
+++ dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/impl/DemoServerImpl.java
@@ -8,8 +8,10 @@ import java.util.Date;
 
 
 public class DemoServerImpl implements DemoServer {
+
     @Override
     public String sayGoodbye(String name) {
+    
         System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return "Dubbo " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
 
--- dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -1,6 +1,9 @@
 package com.alibaba.dubbo.demo.provider;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.net.InetSocketAddress;
 
 /**
  * Created by ken.lj on 2017/7/31.
@@ -10,7 +13,6 @@ public class Provider {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
         context.start();
-
         System.in.read(); // 按任意键退出
     }
 
